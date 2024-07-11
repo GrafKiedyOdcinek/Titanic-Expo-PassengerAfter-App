febff515971fbe6b8c77dc6085cd9b01640b1a30
Mathieu <ceran.mete@gmail.com>
2024-07-11 20:15:16 +0200

    chore: Update OrnementLeft and OrnementRight components styling and remove unnecessary code

--- src/Components/OrnementLeft.jsx
+++ src/Components/OrnementLeft.jsx
@@ -1,29 +1,12 @@
-import { useState, useEffect } from "react";
 import ORNTOPLEFT from "../assets/top-left.png";
 import ORNMID from "../assets/mid-left-right.png";
 import TRAITUP from "../assets/arrow-left-right.png";
 import TRAITDOWN from "../assets/arrow-down-left-right.png";
 
 const OrnementLeft = () => {
-  const [opacity, setOpacity] = useState(1);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const newOpacity = Math.max(1 - scrollY / 300, 0);
-      setOpacity(newOpacity);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className="flex flex-col justify-between fixed left-0 z-10 h-[75%]">
-      <div className="ornement-gauche" style={{ opacity }}>
+      <div className="ornement-gauche">
         <img src={ORNTOPLEFT} alt="Ornement Top Left" />
       </div>
       <div className="traitgauche">
--- src/Components/OrnementRight.jsx
+++ src/Components/OrnementRight.jsx
@@ -2,25 +2,8 @@ import ORNTOPLEFT from "../assets/top-left.png";
 import ORNMID from "../assets/mid-left-right.png";
 import TRAITUP from "../assets/arrow-left-right.png";
 import TRAITDOWN from "../assets/arrow-down-left-right.png";
-import { useEffect, useState } from "react";
 
 const OrnementRight = () => {
-  const [opacity, setOpacity] = useState(1);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const newOpacity = Math.max(1 - scrollY / 300, 0);
-      setOpacity(newOpacity);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className="flex flex-col justify-between fixed right-0 z-10 rotate-180 h-[75%]">
       <div className="traitgauche">
@@ -32,10 +15,7 @@ const OrnementRight = () => {
       <div className="traitdroit">
         <img src={TRAITDOWN} alt="" />
       </div>
-      <div
-        className="ornement-gauche transform scale-y-[-1]"
-        style={{ opacity }}
-      >
+      <div className="ornement-gauche transform scale-y-[-1]">
         <img src={ORNTOPLEFT} alt="" />
       </div>
     </div>
